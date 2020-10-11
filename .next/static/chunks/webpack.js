772a115ff3a2a232ef3a2ff2032900b35f424e44
Pedro Augusto <pedroaugusto@jera.com.br>
2020-10-10 22:27:14 -0400

    [FIX] Build

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0cdb296509ad44f80313";
+/******/ 	var hotCurrentHash = "719561f0cf9a7b6dcf7b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
